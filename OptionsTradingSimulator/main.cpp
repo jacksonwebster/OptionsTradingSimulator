be4254119c61e10b123021ad5cc5084747126012
jacksonhwebster <jacksonhwebster@gmail.com>
2023-04-06 14:36:47 -0400

    working on repl

--- OptionsTradingSimulator/main.cpp
+++ OptionsTradingSimulator/main.cpp
@@ -2,13 +2,26 @@
 //  main.cpp
 //  OptionsTradingSimulator
 //
-//  Created by Jackson Webster on 4/5/23.
+//  Created by Jackson Webster on 4/6/23.
 //
 
+#include <stdio.h>
 #include <iostream>
+#include <map>
+#include <string>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include <cpprest/http_client.h>
+#include <cpprest/json.h>
+
+using namespace web;
+using namespace web::http;
+using namespace web::http::client;
+using namespace web::json;
+
+
+
+int main() {
+    std::map<std::string, void (*)(const std::vector<std::string>&)> repl_functions;
+    repl_functions["GET"] = get_options_contract;
 }
